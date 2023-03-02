015604c7d68c242012902bbf0efa27a7f99766ca
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2023-03-02 17:45:18 +0530

    Update _app.tsx

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -21,7 +21,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Script
         id="cookieyes"
-        src="https://scriptstaging.cookieyes.com/client_data/5870836bed6848a78b305757/script.js"
+        src="https://scriptstaging.cookieyes.com/client_data/ba82a9cbfebfd45694622e4d/script.js"
         strategy="beforeInteractive"
       />
 
